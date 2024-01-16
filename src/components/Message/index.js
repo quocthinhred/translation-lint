0c7f0d78a795b14d384c837f9e2bb5904545ba5c
Kuokkutin <tranquocthinh.t2.15cla@gmail.com>
2024-01-16 19:29:54 +0700

    optimize

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,8 +1,9 @@
 import { defineMessage, defineMessages } from '@/utils/translation';
 import React from 'react'
+import FormattedMessage from '../FormattedMessage';
 
 const message = defineMessage({
-  id: "src.components.Message.",
+  id: "src.components.Message.hello3",
   defaultMessage: "default message 1"
 })
 
@@ -21,8 +22,10 @@ function Message() {
   const hehe = 123;
   return (
     <div>
-      <FormattedMessage id="src.components.Message.">Message</FormattedMessage>
+      <FormattedMessage id="src.components.Message.abcd">Message</FormattedMessage>
       <div><FormattedMessage defaultMessage="Hello I'm Andy" description="" id="src.components.Message.11111" /></div>
+      <FormattedMessage id="src.components.Message.btnBack" defaultMessage="Trở về" />
+      <FormattedMessage id="src.components.Message.btnNext" defaultMessage="Tiếp theo" />
     </div>
   )
 }
